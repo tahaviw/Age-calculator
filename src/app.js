a6987b063a651fe356eda729cebe4b9e3efc0a25
Taha <tahaviwtahtouhihihi@gmail.com>
2026-07-06 23:18:53 +0100

    working on calculation process

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const calculateBtn = document.querySelector("#calculateBtn")
+
+calculateBtn.addEventListener("click", function calculate() {
+    const birthDateInput = document.querySelector("#birthdate").value
+})
