5c66c9bb4ab6b26c45476176b0e8de91d9e997ba
Taha <tahaviwtahtouhihihi@gmail.com>
2026-07-07 23:20:28 +0100

    adding connections

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
+import dayjs from 'dayjs'
 const calculateBtn = document.querySelector("#calculateBtn")
 
 calculateBtn.addEventListener("click", function calculate() {
     const birthDateInput = document.querySelector("#birthdate").value
+    const today = dayjs()
+
+    const age = today.diff(birthDateInput, 'year')
+    console.log(age)
 })
