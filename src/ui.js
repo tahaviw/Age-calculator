93cac3b810a3e5fb3ffd5066d122acfe48a9fd1c
Taha <tahaviwtahtouhihihi@gmail.com>
2026-07-17 00:08:50 +0100

    fix text in results

--- src/ui.js
+++ src/ui.js
@@ -7,9 +7,9 @@ export function renderResults() {
         resultsContainer.innerHTML = '<p class="status-msg">Please enter a valid date !</p>'
     }else {
         resultsContainer.innerHTML = `
-        <p class="ageDisplay">You are ${result.age} old, old enough !!</p>
-        <p class="daysAliveDisplay">You've been alive for ${result.daysAlive}, DAMN !</p>
-        <p class="nextBirthdayDisplay">Be ready your NEXT birthday is in ${result.daysUntilBirthday}</p>
+        <p class="ageDisplay">You are ${result.age} years old, old enough !!</p>
+        <p class="daysAliveDisplay">You've been alive for ${result.daysAlive} days, DAMN !</p>
+        <p class="nextBirthdayDisplay">Be ready your NEXT birthday is in ${result.daysUntilBirthday} days</p>
         `
     }
 }
